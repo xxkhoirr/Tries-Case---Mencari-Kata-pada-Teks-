6b4faefe00b7348acf1de0ab007bc3d2d6475d34
xxkhoirr <64024502+xxkhoirr@users.noreply.github.com>
2020-11-19 21:25:01 +0700

    Add files via upload

---
+++ SDAII_Tries_Mahasiswa/src/HashTrie.java
@@ -0,0 +1,25 @@
+
+import java.util.HashMap;
+
+public class HashTrie {
+    private HashMap<Character, HashMap> root;
+ 
+    /** Constructor **/
+    public HashTrie() {
+       root = new HashMap<>();
+    }
+ 
+    /** Function to add a string to hash trie **/
+    public void add(String str) {
+        
+    }
+ 
+    /** Function to check if hash trie contains a given word **/
+    public boolean contains(String str) {
+                 
+    }
+    
+    public void printTries() {
+        
+    }
+}
---
+++ SDAII_Tries_Mahasiswa/src/Home2.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+public class Home2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        HashTrie trie = new HashTrie();
+        System.out.println("Welcome in Program TRIES :");
+        int pilih = 0;
+        do {
+            System.out.println("Silahkan Pilih :\n1. Insert\n2. Print\n3. Search");
+            System.out.print("Pilih : ");
+            
+        }
+        while(pilih != 4);
+    }
+}
